d3504d6a09f07106eca5928b80e478e28ef07b18
yangzhie <arnavmalasi2212@gmail.com>
2025-10-12 13:39:48 +1100

    Initial commit - blueprint.

---
+++ src/electron/main.ts
@@ -0,0 +1,15 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { isDev } from "./utils.js";
+
+app.on("ready", () => {
+    const mainWindow = new BrowserWindow({});
+
+    if (isDev()) {
+        mainWindow.loadURL('http://localhost:9999/')
+    } else {
+        // Load initial file inside mainWindow
+        // path helps configure Windows' \
+        mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
+    }
+})
