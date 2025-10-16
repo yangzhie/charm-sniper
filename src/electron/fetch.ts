490fb931ed246c6ad322ab9a6b0f76760ad72278
yangzhie <arnavmalasi2212@gmail.com>
2025-10-17 10:23:59 +1100

    Use CSFloat API to check listings on CSFloat platform.

--- src/electron/fetch.ts
+++ src/electron/fetch.ts
@@ -1,11 +1,13 @@
 import "dotenv/config"; 
+import { URLSearchParams } from "url";
 
-const steamSessionID = process.env.steamSessionID
-const steamLoginSecure = process.env.steamLoginSecure
+const steamSessionID = process.env.steamSessionID;
+const steamLoginSecure = process.env.steamLoginSecure;
+const csfloatAPIKey = process.env.csfloatAPIKey;
 
 // @ts-ignore
 // Get the inspect link of item from Steam
-export const fetchInspectLinkFromSteam = async (itemName, pageCount = 2) => {
+export const fetchInspectLinkFromSteam = async (itemName, pageCount = 10) => {
     // Page count: 1 = cheapest, 10 = latest 
     const steamURL = `https://steamcommunity.com/market/listings/730/${itemName}/render?count=${pageCount}&start=0&currency=1&country=US&language=english`;
 
@@ -83,3 +85,37 @@ export const fetchInspectDataFromAPI = async (inspectLink) => {
     }
 }
 
+// @ts-ignore
+// Fetch CSFloat data from CSFloat API
+export const fetchFromCSFloat = async (limit, sort, minFloat, maxFloat, paintSeed, type, marketHashName) => {
+    const csfloatURL = new URL("https://csfloat.com/api/v1/listings");
+    const params = new URLSearchParams();
+
+    if(limit !== undefined) {params.append("limit", limit.toString())};
+    if(sort) {params.append("sort_by", sort)};
+    if(minFloat !== undefined) {params.append("min_float", minFloat.toString())};
+    if(maxFloat !== undefined) {params.append("max_float", maxFloat.toString())};
+    if(paintSeed !== undefined) {params.append("paint_seed", paintSeed.toString())};
+    if(type !== undefined) {params.append("type", type)};
+    if(marketHashName !== undefined) {params.append("market_hash_name", marketHashName)};
+
+    csfloatURL.search = params.toString();
+
+    try {
+        const res = await fetch (csfloatURL, {
+            // @ts-ignore
+            headers: {
+                "Authorization": process.env.csfloatAPIKey
+            }
+        });
+
+        if(!res.ok) {
+            throw new Error(`Err: ${res.status}`);
+        }
+
+        const data = await res.json();
+        console.log(data)
+    } catch(err) {
+        console.error(err);
+    }
+}
--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,7 +1,7 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 import { isDev } from "./utils.js";
-import { fetchInspectLinkFromSteam, fetchInspectDataFromAPI } from "./fetch.js";
+import { fetchInspectLinkFromSteam, fetchInspectDataFromAPI, fetchFromCSFloat } from "./fetch.js";
 
 app.on("ready", () => {
     const mainWindow = new BrowserWindow({});
@@ -14,21 +14,29 @@ app.on("ready", () => {
         mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
     }
 
-    // const itemName = "Charm%20%7C%20Lil'%20Zen";
-    const itemName = "Charm%20%7C%20Lil'%20Facelift";
-    const pageCount = 10; // 1, 10
+    // // const itemName = "Charm%20%7C%20Lil'%20Zen";
+    // const itemName = "Charm%20%7C%20Lil'%20Facelift";
+    // const pageCount = 10; // 1, 10
 
+    // async function temp() {
+    //     const data = await fetchInspectLinkFromSteam(itemName, pageCount);
+    //     // @ts-ignore
+    //     const pattern = fetchInspectDataFromAPI(data?.inspectLink);
+    //     console.log(pattern)
+    //     console.log(data?.latestListingIndex)
+    //     console.log(data?.priceConverted)
+    // }
 
-    async function temp() {
-        const data = await fetchInspectLinkFromSteam(itemName, pageCount);
-        // @ts-ignore
-        const pattern = fetchInspectDataFromAPI(data?.inspectLink);
-        console.log(pattern)
-        console.log(data?.latestListingIndex)
-        console.log(data?.priceConverted)
-    }
+    // setInterval(async () => {
+    //     temp()
+    // }, 10000)
 
-    setInterval(async () => {
-        temp()
-    }, 10000)
+    const limit = 10;
+    const sort = "lowest_price"
+    const minFloat = 0
+    const maxFloat = 1
+    const paintSeed = 1
+    const type = "buy_now"
+    // const marketHashName = ""
+    fetchFromCSFloat(limit, sort, minFloat, maxFloat, paintSeed, type);
 })
