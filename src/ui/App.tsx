72eb9613b88a4ea2c31f97b92a415bb2275f2d20
yangzhie <arnavmalasi2212@gmail.com>
2025-10-21 22:28:15 +1100

    Create boilerplate React code for component Notification.

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,17 +1,22 @@
 import Collection from "./collections/Collection";
+import Notification from "./notifications/Notification";
 
 function App() {
 	return (
 		<>
 			<div className="h-screen box-border overflow-hidden">
-				<div>Charm Sniper v1.0.0</div>
+				<div className="border-white border-b-1 p-1">
+					Charm Sniper v1.0.0
+				</div>
 
 				<div className="flex h-full">
 					<div className="w-2/3 text-center">
 						<Collection />
 					</div>
 
-					<div className="w-1/3"></div>
+					<div className="w-1/3 border-l-1">
+						<Notification />
+					</div>
 				</div>
 			</div>
 		</>
---
+++ src/ui/notifications/Notification.tsx
@@ -0,0 +1,81 @@
+function Notification() {
+	return (
+		<>
+			<div className="h-full w-full">
+				<div className="text-2xl text-center p-2">Notifications</div>
+				<div className="flex flex-col justify-center items-center">
+					<div className="w-40">
+						<img
+							src="https://raw.githubusercontent.com/ByMykel/counter-strike-image-tracker/main/static/panorama/images/econ/keychains/drboom/kc_db_yinyang_png.png"
+							alt=""
+						/>
+					</div>
+					<div className="flex text-lg justify-center items-center w-full p-1 mb-2">
+						Lil' Zen
+					</div>
+				</div>
+				<div>
+					<table className="w-full h-110 flex-col justify-center items-center">
+						<tr className="underline underline-offset-5">
+							<th>Index</th>
+							<th>Pattern</th>
+							<th>Price</th>
+						</tr>
+						<tr>
+							<td className="text-center">1</td>
+							<td className="text-center">88888</td>
+							<td className="text-center">$0.68</td>
+						</tr>
+						<tr>
+							<td className="text-center">2</td>
+							<td className="text-center">88888</td>
+							<td className="text-center">$0.68</td>
+						</tr>
+						<tr>
+							<td className="text-center">3</td>
+							<td className="text-center">88888</td>
+							<td className="text-center">$0.68</td>
+						</tr>
+						<tr>
+							<td className="text-center">4</td>
+							<td className="text-center">88888</td>
+							<td className="text-center">$0.68</td>
+						</tr>
+						<tr>
+							<td className="text-center">5</td>
+							<td className="text-center">88888</td>
+							<td className="text-center">$0.68</td>
+						</tr>
+						<tr>
+							<td className="text-center">6</td>
+							<td className="text-center">88888</td>
+							<td className="text-center">$0.68</td>
+						</tr>
+						<tr>
+							<td className="text-center">7</td>
+							<td className="text-center">88888</td>
+							<td className="text-center">$0.68</td>
+						</tr>
+						<tr>
+							<td className="text-center">8</td>
+							<td className="text-center">88888</td>
+							<td className="text-center">$0.68</td>
+						</tr>
+						<tr>
+							<td className="text-center">9</td>
+							<td className="text-center">88888</td>
+							<td className="text-center">$0.68</td>
+						</tr>
+						<tr>
+							<td className="text-center">10</td>
+							<td className="text-center">88888</td>
+							<td className="text-center">$0.68</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default Notification;
